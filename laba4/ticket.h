3d8d06f2ffc54b07c3e9c654c026c7e9c1473a9d
Maks <maks20081006@gmail.com>
2026-03-22 12:41:23 +0200

    Add ticket

---
+++ laba4/ticket.h
@@ -0,0 +1,44 @@
+#ifndef TICKET_H
+#define TICKET_H
+
+#include <iostream>
+
+using namespace std;
+
+class Ticket {
+private:
+    int price;
+
+public:
+    Ticket(int p = 100) : price(p) {
+        cout << "Ticket constructor was called" << endl;
+    }
+
+    Ticket(const Ticket& other) : price(other.price) {
+        cout << "Ticket copy constructor was called" << endl;
+    }
+
+    Ticket& operator=(const Ticket& rhs) {
+        cout << "Ticket operator= was called" << endl;
+
+        if (this != &rhs) {
+            price = rhs.price;
+        }
+
+        return *this;
+    }
+
+    void setPrice(int p) {
+        price = p;
+    }
+
+    void showPrice() const {
+        cout << "Ticket price: " << price << endl;
+    }
+
+    ~Ticket() {
+        cout << "Ticket destructor was called" << endl;
+    }
+};
+
+#endif
