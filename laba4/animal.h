fdc0d692a28990f47496c01e60a40f877be8f14c
Maks <maks20081006@gmail.com>
2026-03-22 12:38:52 +0200

    Add animal, predator

--- laba4/animal.h
+++ laba4/animal.h
@@ -1,63 +1,62 @@
-#ifndef UNTITLED2_ANIMALS_H
-#define UNTITLED2_ANIMALS_H
+#ifndef ANIMAL_H
+#define ANIMAL_H
 
-#include <iostream>
-#include <string>
+#include "LivingBeing.h"
 
-using namespace std;
-
-class Animal {
-private:
-    string gender;
+class Animal : public LivingBeing {
+protected:
+    string type;
     int weight;
+    string gender;
 
 public:
-    string type;
-    string name;
-
-    Animal(string type = "UnknownType", string name = "UnknownAnimal", int weight = 0, string gender = "Unknown")
-        : gender(gender), weight(weight), type(type), name(name) {}
-
-    Animal(string name) : Animal("UnknownType", name, 0, "Unknown") {}
-    Animal(string name, int weight) : Animal("UnknownType", name, weight, "Unknown") {}
-    Animal(string type, string name, int weight) : Animal(type, name, weight, "Unknown") {}
-
+    Animal(string t = "UnknownType", string n = "UnknownAnimal", int a = 0, int w = 0, string g = "Unknown") : LivingBeing(n, a), type(t), weight(w), gender(g) {
+        cout << "Animal constructor was called" << endl;
+    }
+    Animal(string n) : Animal("UnknownType", n, 0, 0, "Unknown") {}
+    Animal(string n, int w) : Animal("UnknownType", n, 0, w, "Unknown") {}
+    Animal(string t, string n, int w) : Animal(t, n, 0, w, "Unknown") {}
     Animal(const Animal& other)
-        : gender(other.gender), weight(other.weight), type(other.type), name(other.name) {
-        cout << "Copy constructor was called" << endl;
+        : LivingBeing(other), type(other.type), weight(other.weight), gender(other.gender) {
+        cout << "Animal copy constructor was called" << endl;
     }
 
-    void showWeight() const {
-        cout << "Weight = " << weight << endl;
+    Animal& operator=(const Animal& rhs) {
+        cout << "Animal operator= was called" << endl;
+
+        if (this != &rhs) {
+            LivingBeing::operator=(rhs);
+            type = rhs.type;
+            weight = rhs.weight;
+            gender = rhs.gender;
+        }
+
+        return *this;
     }
 
-    void setWeight(int weight) {
-        this->weight = weight;
-        cout << type << " " << name << " weight now = " << weight << endl;
+    void setWeight(int w) {
+        this->weight = w;
     }
 
-    void setGender(string gender) {
-        this->gender = gender;
+    void setGender(string g) {
+        this->gender = g;
     }
 
     void makeSound() const {
-        cout << "say_smt " << name << endl;
+        cout << name << " makes a sound" << endl;
     }
 
-    friend ostream& operator<<(ostream& out, const Animal& a) {
-        out << "Animal -> type: " << a.type
-            << ", name: " << a.name
-            << ", weight: " << a.weight
-            << ", gender: " << a.gender;
-        return out;
+    void showInfo() const override {
+        cout << "Animal: " << name
+             << ", type: " << type
+             << ", age: " << age
+             << ", weight: " << weight
+             << ", gender: " << gender << endl;
     }
 
-    friend istream& operator>>(istream& in, Animal& a) {
-        in >> a.type >> a.name >> a.weight >> a.gender;
-        return in;
+    ~Animal() {
+        cout << "Animal destructor was called" << endl;
     }
-
-    ~Animal() {}
 };
 
 #endif
